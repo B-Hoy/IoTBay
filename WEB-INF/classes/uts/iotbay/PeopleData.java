e928ef5d54b14ef97b9bccf69dc07dea3c038927
AdamMerhi <adam_merhi@icloud.com>
2024-05-08 15:38:43 +1000

    Added register

--- WEB-INF/classes/uts/iotbay/PeopleData.java
+++ WEB-INF/classes/uts/iotbay/PeopleData.java
@@ -9,9 +9,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.io.IOException;
 
-public class Database{
-	Connection conn = null;
-	public Database(){
+public class PeopleData{
+	Connection conn2 = null;
+	public PeopleData(){
 		File temp_file = null;
 		try {
 			Class.forName("org.sqlite.JDBC"); // Need this in case Glassfish doesn't load it automatically
@@ -21,7 +21,7 @@ public class Database{
 		try{
 			temp_file = File.createTempFile("sql", ".db");
 			temp_file.deleteOnExit();
-			conn = DriverManager.getConnection("jdbc:sqlite:" + temp_file.getAbsolutePath());
+			conn2 = DriverManager.getConnection("jdbc:sqlite:" + temp_file.getAbsolutePath());
 			// makes a temp file wherever so it doesn't break on someone else's system
         } catch (SQLException e){
             System.out.println("ERROR: " + e.getMessage());
@@ -30,18 +30,18 @@ public class Database{
 		}
 	}
 	public Connection get_connection(){
-		return this.conn;
+		return this.conn2;
 	}
 	public String[][] driver(){
 		ArrayList<String[]> list = new ArrayList<String[]>();
-		if (conn != null){
+		if (conn2 != null){
 			try{
-				Statement stmt = conn.createStatement();
+				Statement stmt = conn2.createStatement();
 				stmt.setQueryTimeout(5);
 				stmt.executeUpdate("DROP TABLE IF EXISTS test");
 				stmt.executeUpdate("CREATE TABLE test (i1 integer, s1 string)");
-				stmt.executeUpdate("INSERT INTO test VALUES(1, 'kjhguybku')");
-				stmt.executeUpdate("INSERT INTO test VALUES(2, 'FGHJK')");
+				stmt.executeUpdate("INSERT INTO test VALUES(1, 'TEST Person 1')");
+				stmt.executeUpdate("INSERT INTO test VALUES(2, 'TEST Person 2')");
 				ResultSet rs = stmt.executeQuery("SELECT * FROM test");
 				while (rs.next()){
 					list.add(new String[]{rs.getString("i1"), rs.getString("s1")});
@@ -53,9 +53,9 @@ public class Database{
 		return list.toArray(new String[][]{});
 	}
 	public void disconnect(){
-		if (conn != null){
+		if (conn2 != null){
 			try{
-				conn.close();
+				conn2.close();
 			} catch (SQLException e){
 				System.out.println(e.getMessage());
 			}
--- web_pages/hello.jsp
+++ web_pages/hello.jsp
@@ -1,4 +1,6 @@
 <%@page import="uts.iotbay.Database"%>
+<%@page import="uts.iotbay.PeopleData"%>
+
 <html>
 <head>
 <title>JSP Database Testing</title>
@@ -17,14 +19,40 @@ This is the output of a JSP page that is supposed to connect to a SQLite databas
 </tr>
 <%
 Database db = new Database();
-String[][] results = db.driver();
+String[][] dbResults = db.driver();
 db.disconnect();%>
 <table class="test_table">
-	<thead><th>Number</th><th>String</th>
-	<% for (int i = 0; i < results.length; i++){%>
-	<tr><td><%=results[i][0]%></td><td><%=results[i][1]%></td>
+	<thead>
+		<th>Number</th>
+		<th>String</th>
+	</thead>
+	<% for (int i = 0; i < dbResults.length; i++){%>
+	<tr>
+		<td><%=dbResults[i][0]%></td>
+		<td><%=dbResults[i][1]%></td>
+	</tr>
 	<%}%>
 </table>
+
+<br>
+
+<%
+PeopleData pd = new PeopleData();
+String[][] pdResults = pd.driver();
+pd.disconnect();%>
+<table class="test_table2">
+	<thead>
+		<th>Number</th>
+		<th>String</th>
+	</thead>
+	<% for (int i = 0; i < pdResults.length; i++){%>
+	<tr>
+		<td><%=pdResults[i][0]%></td>
+		<td><%=pdResults[i][1]%></td>
+	</tr>
+	<%}%>
+</table>
+
 <%= new String("It Works!") %>
 </body>
 </html>

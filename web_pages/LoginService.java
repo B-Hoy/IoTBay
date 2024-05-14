1f70d0defe4594447f13215a0911eedbe1cff65c
Mousa Raza <mousa.raza777@gmail.com>
2024-05-14 22:08:03 +1000

    deleted LoginService.java

--- web_pages/LoginService.java
+++
@@ -1,9 +0,0 @@
-public class LoginService {
-    public boolean authenticate(Database db, String email, String password) {
-        // Retrieve the user from the database based on the provided email
-        User user = db.get_user(email);
-    
-        // Check if the user exists and if the provided password matches the stored password
-        return user != null && user.get_password().equals(password);
-    }
-}
